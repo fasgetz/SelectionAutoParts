afad685f4ca15f3ab53cfffd0d08aea06aa309fc
fasgetz <fasgetz@yandex.ru>
2020-12-27 05:53:54 +0300

    Init

--- WebSelectionAutoParts/Startup.cs
+++ WebSelectionAutoParts/Startup.cs
@@ -24,6 +24,8 @@ namespace WebSelectionAutoParts
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            int ab = 5;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
